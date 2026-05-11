c2ce9b1107f2fedffcf09c3a9ea4e33b2faccc14
Jyotiraditya Pal <145976232+Beyondverse9K@users.noreply.github.com>
2026-05-12 00:03:24 +0530

    Update CORS configuration to remove localhost
    Removed localhost from CORS allowed origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # CORS configuration for Frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[settings.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
